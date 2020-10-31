751e5d94bd940c4acdded3b6f6a1e2a7bcab17ce
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-10-31 14:50:26 +0500

    Updates

---
+++ precache-manifest.d4054fcda42ee583722a812a3b8ab56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca27df11b00325d29f51f7423a8eebf4",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "cb1ec75ccc7deeca30dd",
+    "url": "/calc/static/css/main.d5dabb58.chunk.css"
+  },
+  {
+    "revision": "a9218aa9154373384fa9",
+    "url": "/calc/static/js/2.1b467fe1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc/static/js/2.1b467fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1ec75ccc7deeca30dd",
+    "url": "/calc/static/js/main.13fe8db5.chunk.js"
+  },
+  {
+    "revision": "3df8d3ea860089294c5c",
+    "url": "/calc/static/js/runtime-main.88bce2da.js"
+  }
+]);
